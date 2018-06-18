151f7a925a4a39e0f96c1adb3d8f31065ac53913
Alexander Volkov <alexandr.volkov@cyberhull.com>
2018-06-18 14:09:07 +0300

    parserfirst test

---
+++ tests/Services/Parsers/Avito/FlatTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @category  Cyberhull
+ * @package   Package
+ * @copyright Copyright (C) 2018 Cyberhull
+ * @author    Alexander Volkov <alexandr.volkov@cyberhull.com>
+ */
+
+namespace App\Tests\Services\Parsers\Avito;
+
+use App\Services\Parsers\Avito\Flat;
+use PHPUnit\Framework\TestCase;
+
+class FlatTest extends TestCase
+{
+    /**
+     * @dataProvider htmlDataProvider
+     */
+    public function testParse($input,$expected)
+    {
+        $parser = new Flat();
+
+        $this->assertEquals($expected, $parser->parse($input));
+    }
+
+    public function htmlDataProvider()
+    {
+        $flat = file_get_contents(__DIR__ . '/flat.html');
+
+        return [
+            [$flat, ['title' => '2-к квартира, 40 м², 6/9 эт.']],
+        ];
+    }
+
+}
