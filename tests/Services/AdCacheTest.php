550dccfa8af315609ad0427349cc4f5fc9911f38
Alexander Volkov <alexandr.volkov@cyberhull.com>
2018-06-20 19:32:54 +0300

    start to develop cache service  test

---
+++ tests/Services/AdCacheTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @category  Cyberhull
+ * @package   Package
+ * @copyright Copyright (C) 2018 Cyberhull
+ * @author    Alexander Volkov <alexandr.volkov@cyberhull.com>
+ */
+
+namespace App\Tests\Services;
+
+use App\Services\AdCache;
+use PHPUnit\Framework\TestCase;
+
+class AdCacheTest extends TestCase
+{
+
+    public function testDelete()
+    {
+
+    }
+
+    public function testIsDifferent()
+    {
+
+    }
+
+    public function testIsInCache()
+    {
+
+    }
+
+    public function testPut()
+    {
+
+    }
+}
